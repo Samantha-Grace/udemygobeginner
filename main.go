c480df0187cf5090f58f18ce203374d00cef837e
Samantha-Grace <sgrace732@gmail.com>
2021-03-16 13:12:15 -0600

    printing

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var s = "are my favorite places in Utah"
+
+func main() {
+
+	fmt.Println("Price, Tooele, and Provo", s)
+	fmt.Println("My favorite places in Jersey are")
+
+}
+
+func hello() {
+	fmt.Println(s)
+}
---
+++ support.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func jersey() {
+	fmt.Println("Newark, Princeton, Belmar and Seaside")
+}
